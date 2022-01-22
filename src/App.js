9de643d46060419930e94fa342bd7c58289173a6
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-01-22 23:46:23 +0100

    add update MultiForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import MultiForm from './Components/MultiForm/MultiForm'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">
+    <MultiForm />
+  </div>;
 }
 
 export default App;
